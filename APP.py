a517da6d6a52a11968a3e7c4ab39364ed2b7fee1
Muhammad Mahad Asim <69845561+mahad-asim76@users.noreply.github.com>
2023-05-17 23:33:46 +0500

    Update APP.py

--- APP.py
+++ APP.py
@@ -2,10 +2,11 @@ import pandas as pd
 from flask_cors import CORS
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/btc')
+@app.route('/')
 def btc_time_series_analysis():
     url = 'BTC.csv'
     btc_forecast = pd.read_csv(url)
@@ -13,35 +14,35 @@ def btc_time_series_analysis():
     print(response)
     return response
 
-@app.route('/api/eth')
+@app.route('/eth')
 def eth_time_series_analysis():
     url = 'ETH.csv'
     eth_forecast = pd.read_csv(url)
     response = jsonify(eth_forecast.to_dict(orient='records'))
     return response
 
-@app.route('/api/ada')
+@app.route('/ada')
 def ada_time_series_analysis():
     url = 'ADA.csv'
     ada_forecast = pd.read_csv(url)
     response = jsonify(ada_forecast.to_dict(orient='records'))
     return response
 
-@app.route('/api/dot')
+@app.route('/dot')
 def dot_time_series_analysis():
     url = 'DOT.csv'
     ada_forecast = pd.read_csv(url)
     response = jsonify(ada_forecast.to_dict(orient='records'))
     return response
 
-@app.route('/api/sol')
+@app.route('/sol')
 def sol_time_series_analysis():
     url = 'SOL.csv'
     ada_forecast = pd.read_csv(url)
     response = jsonify(ada_forecast.to_dict(orient='records'))
     return response
 
-@app.route('/api/matic')
+@app.route('/matic')
 def matic_time_series_analysis():
     url = 'MATIC.csv'
     ada_forecast = pd.read_csv(url)
@@ -50,4 +51,4 @@ def matic_time_series_analysis():
 
 # Run the application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run()
